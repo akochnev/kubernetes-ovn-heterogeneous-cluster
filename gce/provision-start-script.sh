9196e63c382490100055dae0694724ffd9cfc64a
Bob Steciuk <bsteciuk@apprenda.com>
2017-05-03 19:19:31 -0400

    Added provisioning and configuration of linux nodes for GCE

---
+++ gce/provision-start-script.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+if [[ -f "/setup" ]]; then
+    touch /ready
+    exit 0
+fi
+#OVS/OVN Installation
+curl -fsSL https://yum.dockerproject.org/gpg | apt-key add -
+echo "deb https://apt.dockerproject.org/repo ubuntu-xenial main" > sudo tee /etc/apt/sources.list.d/docker.list
+
+apt update
+apt install -y docker.io dkms
+
+cd ~
+git clone https://github.com/bsteciuk/kubernetes-ovn-heterogeneous-cluster
+cd kubernetes-ovn-heterogeneous-cluster/deb
+
+dpkg -i openvswitch-common_2.6.2-1_amd64.deb \
+openvswitch-datapath-dkms_2.6.2-1_all.deb \
+openvswitch-switch_2.6.2-1_amd64.deb \
+ovn-common_2.6.2-1_amd64.deb \
+ovn-central_2.6.2-1_amd64.deb \
+ovn-docker_2.6.2-1_amd64.deb \
+ovn-host_2.6.2-1_amd64.deb \
+python-openvswitch_2.6.2-1_all.deb
+
+echo vport_geneve >> /etc/modules-load.d/modules.conf
+
+touch /setup
+echo "Rebooting system now."
+reboot
